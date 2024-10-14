646c94e20a90bbd469bcafc69cc31726ee9afbb4
Akmal Eka FIrlana <138485072+akmalreal@users.noreply.github.com>
2024-10-14 13:11:10 +0700

    getting tired

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @vite('resources/css/app.css')
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       
 
 
     </head>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,8 +26,8 @@
                     </x-nav-link>
                 </div>
 
-                <li class="nav-item">
-                <form class="form-s">
+                
+                <form class="form-s mx-4 mt-3">
                     <button>
                         <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img"
                             aria-labelledby="search">
@@ -44,7 +44,7 @@
                         </svg>
                     </button>
                 </form>
-            </li>
+            
             
             </div>
 
@@ -83,7 +83,7 @@
                 </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
+            <!-- Hamburger --
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -91,7 +91,7 @@
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
-            </div>
+            </div>-->
         </div>
     </div>
 
